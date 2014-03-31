e28530ad250a2c139caec6ec1b96ca375cd4651f
Chris Bautista <chrisbjr@gmail.com>
2014-04-01 00:54:07 +0800

    Added PagerSlidingTabStrip library

--- pflix/src/main/java/me/chrisbautista/android/pflix/MainActivity.java
+++ pflix/src/main/java/me/chrisbautista/android/pflix/MainActivity.java
@@ -1,20 +1,56 @@
 package me.chrisbautista.android.pflix;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 
-import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.astuetz.PagerSlidingTabStrip;
 
-public class MainActivity extends SherlockActivity {
+import me.chrisbautista.android.pflix.fragments.TestFragment;
+
+public class MainActivity extends SherlockFragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().setTitle("Hello pFlix");
+        getSupportActionBar().setTitle("pFlix");
+
+        // Initialize the ViewPager and set an adapter
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+
+        // Bind the tabs to the ViewPager
+        PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+        tabs.setViewPager(pager);
+    }
+
+    public class MyPagerAdapter extends FragmentPagerAdapter {
+
+        private final String[] TITLES = {"Favorite Malls", "Now Showing", "Malls"};
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return TITLES[position];
+        }
+
+        @Override
+        public int getCount() {
+            return TITLES.length;
+        }
+
+        @Override
+        public TestFragment getItem(int position) {
+            return TestFragment.newInstance(position);
+        }
+
     }
 
 }
---
+++ pflix/src/main/java/me/chrisbautista/android/pflix/fragments/TestFragment.java
@@ -0,0 +1,60 @@
+package me.chrisbautista.android.pflix.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by chrisbjr on 3/31/14.
+ */
+public class TestFragment extends Fragment {
+
+    private static final String ARG_POSITION = "position";
+
+    private int position;
+
+    public static TestFragment newInstance(int position) {
+        TestFragment f = new TestFragment();
+        Bundle b = new Bundle();
+        b.putInt(ARG_POSITION, position);
+        f.setArguments(b);
+        return f;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        position = getArguments().getInt(ARG_POSITION);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+
+        FrameLayout fl = new FrameLayout(getActivity());
+        fl.setLayoutParams(params);
+
+        final int margin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, getResources()
+                .getDisplayMetrics());
+
+        TextView v = new TextView(getActivity());
+        params.setMargins(margin, margin, margin, margin);
+        v.setLayoutParams(params);
+        v.setLayoutParams(params);
+        v.setGravity(Gravity.CENTER);
+        v.setText("CARD " + (position + 1));
+
+        fl.addView(v);
+        return fl;
+    }
+
+}
